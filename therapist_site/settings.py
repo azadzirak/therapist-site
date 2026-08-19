2785f1ace9459d0e28ae3359310375018f94c6ed
azad zirak <azad.damiarzirak@gmail.com>
2026-08-20 03:25:00 +0330

    settings5

--- therapist_site/settings.py
+++ therapist_site/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'therapist_site.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'database.db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "database" / "db.sqlite3",
     }
 }
 
